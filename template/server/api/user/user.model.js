770cff136f28b534b6ec03f03bd0bc004f4fbeed
erguotou <erguotou525@gmail.com>
2017-09-26 17:16:13 +0800

    Update user.model.js

--- template/server/api/user/user.model.js
+++ template/server/api/user/user.model.js
@@ -136,7 +136,7 @@ UserSchema.methods = {
   encryptPassword: function (password) {
     if (!password || !this.salt) return ''
     var salt = new Buffer(this.salt, 'base64')
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64')
+    return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64')
   }
 }
 
